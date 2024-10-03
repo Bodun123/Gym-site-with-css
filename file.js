0f85a722a7d4e3d65b79b92aabad3bfeff7de1c7
Bodun123 <106692823+Bodun123@users.noreply.github.com>
2024-10-03 08:15:17 +0100

    Add files via upload

---
+++ file.js
@@ -0,0 +1,8 @@
+/*Визначив тип та версію браузера*/
+window.onload = function() {
+    let userAgent = navigator.userAgent;
+    let browserName = navigator.appName;
+    let browserVersion = navigator.appVersion;
+    let output = "Браузер: " + browserName + ", Версія: " + browserVersion + ", UserAgent: " + userAgent;
+    document.write(output);
+};
